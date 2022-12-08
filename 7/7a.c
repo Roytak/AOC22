9808b711ebcd84b06ba4f892d36c0c7c7d446114
Roytak <xjanot04@fit.vutbr.cz>
2022-12-08 17:41:30 +0100

    day 7

--- 7/7a.c
+++ 7/7a.c
@@ -14,13 +14,11 @@ struct file {
 struct directory {
 	char *name;
 
-	struct directory *parent_dir;
+	struct directory *parent;
+	struct directory *child;
+	struct directory *next;
 
-	struct directory *sub_directories;
-	int sub_directory_cnt;
-
-	struct file *files;
-	int file_cnt;
+	unsigned long file_size;
 };
 
 void
@@ -34,12 +32,25 @@ null_buf(char buffer[BUFSIZE])
 }
 
 struct directory *
+create_direcory(char *dir_name, struct directory *parent)
+{
+	struct directory *dir;
+
+	dir = calloc(1, sizeof *dir);
+	if (dir) {
+		dir->name = strdup(dir_name);
+		dir->parent = parent;
+	}
+	return dir;
+}
+
+struct directory *
 decode_input(char buffer[BUFSIZE], struct directory *current)
 {
 	char *new_dir;
-	char file_name[256] = {0};
 	int file_size = 0;
-	int i;
+	struct directory *iter = NULL;
+	struct directory *new = NULL;
 
 	if (buffer[0] == '$') {
 		if (buffer[2] == 'c') {
@@ -47,65 +58,60 @@ decode_input(char buffer[BUFSIZE], struct directory *current)
 			new_dir = buffer + 5;
 			//printf("%s\n", new_dir);
 			if (!strcmp(new_dir, "..")) {
-				current = current->parent_dir;
+				current = current->parent;
 			} else if (!strcmp(new_dir, "/")) {
 				while (strcmp(current->name, new_dir)) {
-					current = current->parent_dir;
+					current = current->parent;
 				}
 			} else {
-				for (i = 0; i < current->sub_directory_cnt; i++) {
-					if (!strcmp(new_dir, current->sub_directories[i].name)) {
-						current = &current->sub_directories[i];
-						break;
-					}
+				current = current->child;
+				while (strcmp(new_dir, current->name)) {
+					current = current->next;
 				}
 			}
 		}
 	} else if (buffer[0] == 'd') {
 		/* dir */
-		//printf("%s\n", buffer + 4);
-		current->sub_directories = realloc(current->sub_directories, (current->sub_directory_cnt + 1) * sizeof *current->sub_directories);
-		memset(&current->sub_directories[current->sub_directory_cnt], 0, sizeof *current->sub_directories);
-		current->sub_directories[current->sub_directory_cnt].name = strdup(buffer + 4);
-		current->sub_directories[current->sub_directory_cnt].parent_dir = current;
-		current->sub_directory_cnt++;
+		new = create_direcory(buffer + 4, current);
+		if (!current->child) {
+			current->child = new;
+		} else {
+			iter = current->child;
+			while (iter->next) {
+				iter = iter->next;
+			}
+			iter->next = new;
+		}
 	} else {
 		/* file */
-		current->files = realloc(current->files, (current->file_cnt + 1) * sizeof *current->files);
-		memset(&current->files[current->file_cnt], 0, sizeof *current->files);
-		sscanf(buffer, "%d %s", &file_size, file_name);
-		current->files[current->file_cnt].name = strdup(file_name);
-		current->files[current->file_cnt].size = file_size;
-		current->file_cnt++;
+		sscanf(buffer, "%d", &file_size);
+		current->file_size += file_size;
 	}
 
+	//printf("name: %s fsize %ld\n", current->name, current->file_size);
 	return current;
 }
 
-int
-compute_sum(struct directory *current, int *final)
+unsigned long long
+tree_sum(struct directory *tree, unsigned long long *final_sum)
 {
-	int sum = 0, i, j, sub_dir_sum = 0;
+	unsigned long long sum = 0;
 
-	if (!current) {
+	if (!tree) {
 		return 0;
 	}
 
-	for (i = 0; i < current->sub_directory_cnt; i++) {
-		sub_dir_sum += compute_sum(&current->sub_directories[i], final);
-		//printf("subd %s size %d\n", current->sub_directories[i].name, dir_sum);
+	sum += tree_sum(tree->child, final_sum);
+
+	sum += tree->file_size;
+
+	if (sum < MAX_FILE_SIZE) {
+		*final_sum += sum;
 	}
 
-	//printf("subd %s size %d\n", current->name, sub_dir_sum);
+	sum += tree_sum(tree->next, final_sum);
 
-	for (j = 0; j < current->file_cnt; j++) {
-		sum += current->files[j].size;
-	}
-
-	if (sum + sub_dir_sum < MAX_FILE_SIZE) {
-		*final += sum + sub_dir_sum;
-	}
-	//printf("curr %s sum %d\n", current->name, sum+sub_dir_sum);
+	printf("current %s sum %llu\n", tree->name, *final_sum);
 
 	return sum;
 }
@@ -119,13 +125,12 @@ main(void)
 	}
 
 	char buffer[BUFSIZE];
-	int sum = 0;
+	unsigned long long sum = 0;
 	struct directory *current;
-	struct directory *home = calloc(1, sizeof *home);
+	struct directory *home = create_direcory("/", NULL);
 	if (!home) {
 		return 1;
 	}
-	home->name = strdup("/");
 
 	current = home;
 	while (fgets(buffer, BUFSIZE, f) != NULL) {
@@ -137,8 +142,9 @@ main(void)
 		null_buf(buffer);
 	}
 
-	compute_sum(home, &sum);
-	printf("%d\n", sum);
+	//compute_sum(home, &sum);
+	tree_sum(home, &sum);
+	printf("%llu\n", sum);
 
 	return 0;
 }
--- 7/7b.c
+++ 7/7b.c
@@ -3,6 +3,141 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define BUFSIZE 256
+#define MAX_FILE_SIZE 100000
+
+struct file {
+	char *name;
+	int size;
+};
+
+struct directory {
+	char *name;
+
+	struct directory *parent;
+	struct directory *child;
+	struct directory *next;
+
+	unsigned long file_size;
+};
+
+void
+null_buf(char buffer[BUFSIZE])
+{
+	int i;
+
+	for (i = 0; i < BUFSIZE; i++) {
+		buffer[i] = 0;
+	}
+}
+
+struct directory *
+create_direcory(char *dir_name, struct directory *parent)
+{
+	struct directory *dir;
+
+	dir = calloc(1, sizeof *dir);
+	if (dir) {
+		dir->name = strdup(dir_name);
+		dir->parent = parent;
+	}
+	return dir;
+}
+
+struct directory *
+decode_input(char buffer[BUFSIZE], struct directory *current)
+{
+	char *new_dir;
+	int file_size = 0;
+	struct directory *iter = NULL;
+	struct directory *new = NULL;
+
+	if (buffer[0] == '$') {
+		if (buffer[2] == 'c') {
+			/* cd */
+			new_dir = buffer + 5;
+			//printf("%s\n", new_dir);
+			if (!strcmp(new_dir, "..")) {
+				current = current->parent;
+			} else if (!strcmp(new_dir, "/")) {
+				while (strcmp(current->name, new_dir)) {
+					current = current->parent;
+				}
+			} else {
+				current = current->child;
+				while (strcmp(new_dir, current->name)) {
+					current = current->next;
+				}
+			}
+		}
+	} else if (buffer[0] == 'd') {
+		/* dir */
+		new = create_direcory(buffer + 4, current);
+		if (!current->child) {
+			current->child = new;
+		} else {
+			iter = current->child;
+			while (iter->next) {
+				iter = iter->next;
+			}
+			iter->next = new;
+		}
+	} else {
+		/* file */
+		sscanf(buffer, "%d", &file_size);
+		current->file_size += file_size;
+	}
+
+	//printf("name: %s fsize %ld\n", current->name, current->file_size);
+	return current;
+}
+
+unsigned long long
+tree_sum(struct directory *tree, unsigned long long *final_sum)
+{
+	unsigned long long sum = 0;
+
+	if (!tree) {
+		return 0;
+	}
+
+	sum += tree_sum(tree->child, final_sum);
+
+	sum += tree->file_size;
+
+	sum += tree_sum(tree->next, final_sum);
+
+	if (!strcmp(tree->name, "/")) {
+		*final_sum = sum;
+	}
+
+	return sum;
+}
+
+unsigned long long
+find_smallest(struct directory *tree, unsigned long long required_space, unsigned long long *min)
+{
+	unsigned long long sum = 0;
+
+	if (!tree) {
+		return 0;
+	}
+
+	sum += find_smallest(tree->child, required_space, min);
+
+	sum += tree->file_size;
+
+	if (sum >= required_space) {
+		if (sum < *min) {
+			*min = sum;
+		}
+	}
+
+	sum += find_smallest(tree->next, required_space, min);
+
+	return sum;
+}
+
 int
 main(void)
 {
@@ -11,6 +146,32 @@ main(void)
 		return 1;
 	}
 
+	char buffer[BUFSIZE];
+	unsigned long long sum = 0;
+	unsigned long long req_space = 0;
+	unsigned long long ans = 70000000;
+	struct directory *current;
+	struct directory *home = create_direcory("/", NULL);
+	if (!home) {
+		return 1;
+	}
+
+	current = home;
+	while (fgets(buffer, BUFSIZE, f) != NULL) {
+		if (buffer[strlen(buffer) - 1] == '\n') {
+			buffer[strlen(buffer) - 2] = 0;
+		}
+		//printf("%s\n", buffer);
+		current = decode_input(buffer, current);
+		null_buf(buffer);
+	}
+
+	//compute_sum(home, &sum);
+	tree_sum(home, &sum);
+	req_space = 30000000 - (70000000  - sum);
+	printf("%llu\n", req_space);
+	find_smallest(home, req_space, &ans);
+	printf("ans: %llu\n", ans);
 
 	return 0;
 }
